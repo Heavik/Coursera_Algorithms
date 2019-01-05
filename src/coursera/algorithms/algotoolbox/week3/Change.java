f391e85b085c03826c6949ad43f9a235f8cf056c
Mikhail Kharchenko <kharchenkoms@gmail.com>
2019-01-05 13:55:10 +0200

    added assigment for problem 1 week 3

---
+++ src/coursera/algorithms/algotoolbox/week3/Change.java
@@ -0,0 +1,27 @@
+//package coursera.algorithms.algotoolbox.week3;
+
+import java.util.Scanner;
+
+public class Change {
+    private static int getChange(int m) {
+        
+        final int ten = 10, five = 5, one = 1;
+        int changes = 0;
+        
+        while(m > 0) {
+            if (m >= ten) m -= ten;
+            else if (m >= five) m -= five;
+            else m -= one;
+            
+            changes++;
+        }
+        
+        return changes;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        System.out.println(getChange(m));
+    }
+}
